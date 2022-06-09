df11184ef118baa37e219f652b2b01a9b3f605c8
garry-rogov <garry.rogov@gmail.com>
2022-06-09 07:27:27 +0300

    vendorsetup: Delete android_vendor_nxp_opensource_packages_apps_Nfc
    Delete android_vendor_nxp_opensource_packages_apps_Nfc

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,6 @@ rm -rf vendor/qcom/opensource/interfaces
 rm -rf vendor/nxp/opensource/interfaces/nfc
 rm -rf vendor/nxp/opensource/commonsys/external/libnfc-nci
 rm -rf vendor/nxp/opensource/commonsys/frameworks
-rm -rf vendor/nxp/opensource/commonsys/packages/apps/Nfc
 rm -rf vendor/nxp/opensource/sn100x/halimpl
 rm -rf vendor/nxp/opensource/sn100x/hidlimpl
 git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-12.1-caf-sm8150 hardware/qcom-caf/sm8150/display
@@ -14,7 +13,6 @@ git clone https://github.com/ArrowOS/android_hardware_qcom_media.git -b arrow-12
 git clone https://github.com/ArrowOS/android_vendor_nxp_interfaces_opensource_nfc.git -b arrow-12.1 vendor/nxp/opensource/interfaces/nfc
 git clone https://github.com/ArrowOS/android_vendor_nxp_opensource_external_libnfc-nci.git -b arrow-12.1 vendor/nxp/opensource/commonsys/external/libnfc-nci
 git clone https://github.com/ArrowOS/android_vendor_nxp_opensource_frameworks.git -b arrow-12.1 vendor/nxp/opensource/commonsys/frameworks
-git clone https://github.com/ArrowOS/android_vendor_nxp_opensource_packages_apps_Nfc.git -b arrow-12.1 vendor/nxp/opensource/commonsys/packages/apps/Nfc
 git clone https://github.com/ArrowOS/android_vendor_nxp_opensource_halimpl.git -b arrow-12.1-sn100x vendor/nxp/opensource/sn100x/halimpl
 git clone https://github.com/ArrowOS/android_vendor_nxp_opensource_hidlimpl.git -b arrow-12.1-sn100x vendor/nxp/opensource/sn100x/hidlimpl
 git clone https://github.com/ArrowOS/android_vendor_qcom_opensource_interfaces.git -b arrow-12.1 vendor/qcom/opensource/interfaces
