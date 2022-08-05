780f754565a30e737a23bd466963ba60891e1b98
garry-rogov <garry.rogov@gmail.com>
2022-08-05 10:28:17 +0300

    willow: Update DRM/Clearkey HIDL to v1.4

--- device.mk
+++ device.mk
@@ -152,7 +152,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.4.vendor
 
 # Fingerprint
 PRODUCT_PACKAGES += \
