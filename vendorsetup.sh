0e08c626058748c4c0f09a8d2470a5adac7821cf
garry-rogov <garry.rogov@gmail.com>
2022-08-27 06:58:21 +0300

    willow: vendorsetup: switch NFC to crDroid

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,19 +1,6 @@
 rm -rf hardware/qcom-caf/sm8150/display
 rm -rf hardware/qcom-caf/sm8150/media
 rm -rf hardware/qcom-caf/sm8150/audio
-rm -rf vendor/qcom/opensource/interfaces
-rm -rf vendor/nxp/opensource/interfaces/nfc
-rm -rf vendor/nxp/opensource/commonsys/external/libnfc-nci
-rm -rf vendor/nxp/opensource/commonsys/frameworks
-rm -rf vendor/nxp/opensource/sn100x/halimpl
-rm -rf vendor/nxp/opensource/sn100x/hidlimpl
 git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-12.1-caf-sm8150 hardware/qcom-caf/sm8150/display
 git clone https://github.com/ArrowOS/android_hardware_qcom_audio.git -b arrow-12.1-caf-sm8150 hardware/qcom-caf/sm8150/audio
 git clone https://github.com/ArrowOS/android_hardware_qcom_media.git -b arrow-12.1-caf-sm8150 hardware/qcom-caf/sm8150/media
-git clone https://github.com/ArrowOS/android_vendor_nxp_interfaces_opensource_nfc.git -b arrow-12.1 vendor/nxp/opensource/interfaces/nfc
-git clone https://github.com/ArrowOS/android_vendor_nxp_opensource_external_libnfc-nci.git -b arrow-12.1 vendor/nxp/opensource/commonsys/external/libnfc-nci
-git clone https://github.com/ArrowOS/android_vendor_nxp_opensource_frameworks.git -b arrow-12.1 vendor/nxp/opensource/commonsys/frameworks
-git clone https://github.com/ArrowOS/android_vendor_nxp_opensource_halimpl.git -b arrow-12.1-sn100x vendor/nxp/opensource/sn100x/halimpl
-git clone https://github.com/ArrowOS/android_vendor_nxp_opensource_hidlimpl.git -b arrow-12.1-sn100x vendor/nxp/opensource/sn100x/hidlimpl
-git clone https://github.com/ArrowOS/android_vendor_qcom_opensource_interfaces.git -b arrow-12.1 vendor/qcom/opensource/interfaces
-rm -rf vendor/nxp/opensource/sn100x/halimpl/SN100x/halimpl/libnxpparser
