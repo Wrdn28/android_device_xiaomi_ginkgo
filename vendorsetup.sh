e06d36a3c0b4c67c14f746d15a774609563f437b
garry-rogov <garry.rogov@gmail.com>
2022-09-01 05:16:18 +0300

    willow: vendorsetup: switch audio and media

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,2 @@
 rm -rf hardware/qcom-caf/sm8150/display
-rm -rf hardware/qcom-caf/sm8150/media
-rm -rf hardware/qcom-caf/sm8150/audio
 git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-12.1-caf-sm8150 hardware/qcom-caf/sm8150/display
-git clone https://github.com/ArrowOS/android_hardware_qcom_audio.git -b arrow-12.1-caf-sm8150 hardware/qcom-caf/sm8150/audio
-git clone https://github.com/ArrowOS/android_hardware_qcom_media.git -b arrow-12.1-caf-sm8150 hardware/qcom-caf/sm8150/media
