a794b9f7ff5cecfd554b6a0cb085d06aaee05a9f
garry-rogov <garry.rogov@gmail.com>
2022-10-22 19:27:54 +0300

    willow: don not build Aperture

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -6,7 +6,8 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
     MatLog \
-    bootanimation.zip
+    bootanimation.zip \
+    Aperture
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
