e0b0b3b767182f482942a288f35d250fb9a171aa
garry-rogov <garry.rogov@gmail.com>
2022-10-27 14:22:01 +0300

    willow: Add Translations overlay

--- device.mk
+++ device.mk
@@ -19,6 +19,12 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Overlays - RU translations
+DEVICE_PACKAGE_OVERLAYS += \
+    packages/resources/translations/overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    packages/resources/translations/overlay
+
 PRODUCT_PACKAGES += \
     GinkgoNoCutoutOverlay \
     NotchBarKiller \
