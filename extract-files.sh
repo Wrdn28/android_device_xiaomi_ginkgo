02a94af923b1878f83b0f57f0f16d5284119e2ad
sureshfizzy <sureshfizzy0503@gmail.com>
2022-12-07 18:59:43 +0530

    ginkgo: Import Dolby Atmos from OnePlus 9R
    Co-Authored-By: Anushek Prasal <anushekprasal@gmail.com>
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Co-Authored-By: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-Authored-By: ziasam <ziauddinsameer@gmail.com>
    Co-Authored-By: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: MrTopia <redkarmakar@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,9 @@ function blob_fixup() {
         vendor/lib64/libwvhidl.so | vendor/lib64/mediadrm/libwvdrmengine.so)
 	        "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
 	        ;;
+        vendor/lib/libstagefright_soft_ac4dec.so | vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefrightdolby.so | vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefright_soft_ddpdec.so | vendor/lib64/libstagefrightdolby.so)
+            grep -q "libstagefright_foundation-v33.so" "${2}" || "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
