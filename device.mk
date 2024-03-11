984f26e441472f028d3f8928babc2a0acf953a43
garry-rogov <garry.rogov@gmail.com>
2024-03-11 13:48:16 +0300

    ginkgo/willow: Add ViPER4Android FX support
    * Need to clone this:
    git clone https://github.com/garry-rogov/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
    
    Co-authored-by: mhmdeve <mhmdeveloperofficial@gmail.com>

--- device.mk
+++ device.mk
@@ -456,6 +456,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/input/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# Viper
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
