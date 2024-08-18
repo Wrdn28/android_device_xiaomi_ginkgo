c8df4d422beefe09b302f920be04c921fe029651
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-18 09:52:31 +0300

    ginkgo: skip dependencies which aren't defined in .bp
    Change-Id: I01a653661856495788c05b720c0577dac44998a4
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -66,6 +66,9 @@ function lib_to_package_fixup_vendor_variants() {
             vendor.qti.ims.rcsconfig@1.1)
             echo "${1}_vendor"
             ;;
+            libgrallocutils | \
+            libwpa_client)
+            ;;
         *)
             return 1
             ;;
