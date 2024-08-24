18c793fe2353784c9f917621a6921a505c747847
eklerismunir <munir.nasibzade@proton.me>
2024-08-24 19:53:37 +0400

    ginkgo: Add Miuicamera support Signed-off-by: eklerismunir <munir.nasibzade@proton.me>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -93,7 +93,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Camera
-$(call inherit-product-if-exists, vendor/miuicamera/config.mk)
+$(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
