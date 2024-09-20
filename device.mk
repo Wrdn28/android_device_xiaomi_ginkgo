26e1dff46a36aea4af6c9781e84a78583e9a89d0
Wrdn28 <ifloraxd@gmail.com>
2024-09-20 10:22:25 +0000

    ginkgo: Set path private keys
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, vendor/xiaomi/ginkgo/ginkgo-vendor.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Private Keys
+-include vendor/afterlife-priv/keys/keys.mk
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
