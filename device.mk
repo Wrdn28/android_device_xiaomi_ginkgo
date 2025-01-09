64965a7a0cf2816291d7763d21e49c91941cf5b5
Michael Bestas <mkbestas@lineageos.org>
2025-01-09 18:55:43 -0500

    ginkgo: Update display flags and packages
    Required after display HAL blueprint conversion
    
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -132,8 +132,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     vendor.qti.hardware.memtrack-service \
-    gralloc.trinket \
-    hwcomposer.trinket \
+    hwcomposer.qcom \
+    gralloc.qcom \
     libdisplayconfig.qti \
     libtinyxml \
     libtinyxml2 \
