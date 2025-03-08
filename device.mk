81bc89a956f4e1856e43ec844bdb514b7c3f8a5d
Wrdn28 <ifloraxd@gmail.com>
2025-03-08 14:27:32 +0700

    ginkgo: Import LineageOS overlays
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -19,7 +19,8 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     GinkgoMiuiCamera \
