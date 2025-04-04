0920503d0176774300bfa7881be1195b955afc85
Wrdn28 <ifloraxd@gmail.com>
2025-04-04 16:37:50 +0000

    ginkgo: drop NfcNci
    build/make/core/tasks/platform_availability_check.mk:37: warning:  Following modules are requested to be installed. But are not available for platform because they do not have //apex_available:platform or they depend on other modules that are not available for platform
    Offending entries:
    NfcNci:packages/apps/Nfc
    
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -323,8 +323,7 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
-    NfcNci \
-    Tag
+    Tag 
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
